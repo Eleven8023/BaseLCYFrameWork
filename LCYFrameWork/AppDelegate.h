daf5ca2353f18e823e8b253f60d02c5d9b03456b
frank <15754636787@163.com>
2016-03-05 20:38:38 +0800

    Initial Commit

---
+++ LCYFrameWork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LCYFrameWork
+//
+//  Created by 李赐岩 on 16/3/5.
+//  Copyright © 2016年 李赐岩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LCYFrameWork/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LCYFrameWork
+//
+//  Created by 李赐岩 on 16/3/5.
+//  Copyright © 2016年 李赐岩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
